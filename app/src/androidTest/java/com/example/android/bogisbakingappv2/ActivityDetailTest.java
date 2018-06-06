dc8a14df3312df453bb48faf02da45f547d58fd6
NurithSabo <33427766+NurithSabo@users.noreply.github.com>
2018-06-06 19:54:10 +0200

    bbb

--- app/src/androidTest/java/com/example/android/bogisbakingappv2/ActivityDetailTest.java
+++ app/src/androidTest/java/com/example/android/bogisbakingappv2/ActivityDetailTest.java
@@ -1,8 +1,68 @@
 package com.example.android.bogisbakingappv2;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.is;
+
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import com.example.android.bogisbakingappv2.ActivityDetail;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * Created by Bogi on 2018. 06. 06..
  */
 
 public class ActivityDetailTest {
+
+
+    @Rule
+    public ActivityTestRule<ActivityDetail> mActivityTestRule =
+            new ActivityTestRule<>(ActivityDetail.class);
+
+    @Test
+    public void clickOnRecyclerViewItem() {
+
+        onView(withId(R.id.recipe_details_steps))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0,click()));
+        onView(withId(R.id.content_frame_recipe_details))
+                .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void clickOnStepWithVideo() {
+
+        onView(withId(R.id.recipe_details_steps))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        onView(
+                allOf(
+                        withId(R.id.recipe_step_video),
+                        withClassName(is(SimpleExoPlayer.class.getName()))));
+    }
+
+   @Test
+    public void clickOnStepWithoutVideo() {
+
+        onView(withId(R.id.recipe_details_steps))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+        onView(
+                allOf(
+                        withId(R.id.android_eats_apple_image),
+                        isDisplayed()))
+                .check(matches(isDisplayed()));
+    }
+
 }
